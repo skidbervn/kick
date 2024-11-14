cfcdc6ea1304e3d7bc4c2eda7b9bd1e582ac111a
skidbervn <demopuuogn@gmail.com>
2024-11-15 05:51:35 +0700

    kick.lua

--- kick.lua
+++ kick.lua
@@ -1,1 +1,9 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local helloEvent = ReplicatedStorage:WaitForChild("HelloEvent")
+local Players = game:GetService("Players")
 
+-- Khi sự kiện được kích hoạt, kick người chơi với lý do "Hello"
+helloEvent.OnClientEvent:Connect(function()
+    local player = Players.LocalPlayer
+    player:Kick("Hello")  -- Người chơi sẽ bị kick với lý do "Hello"
+end)
